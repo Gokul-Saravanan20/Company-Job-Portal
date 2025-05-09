98d58abe08619eb93c96083adf897247aba08745
Gokul-Saravanan20 <gokulsaravanan758@gmail.com>
2025-05-09 16:51:39 +0530

    Job Module Completed

---
+++ IdeaProjects/jobApplication-master/src/main/java/com/JobApplication/job/Job.java
@@ -0,0 +1,77 @@
+package com.JobApplication.job;
+
+import com.JobApplication.company.Company;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name="job_table")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tittle;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    @ManyToOne
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(String minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
